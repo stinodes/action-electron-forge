0ee2b6d164c3bebf197b8096b7d51673e4b10631
Stijn Tytgat <stijn@stinodes.dev>
2022-05-15 13:35:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ const runAction = () => {
 
 	// Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
 	setEnv("GH_TOKEN", getInput("github_token", true));
+	setEnv("GITHUB_TOKEN", getInput("github_token", true));
 
 	// Require code signing certificate and password if building for macOS. Export them to environment
 	// variables (required by `electron-builder`)
